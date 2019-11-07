a6db5a060698c933808c94fa74e3e8233e793749
Adam Ali <adamalexali@gmail.com>
2019-11-07 17:56:44 -0500

    added intro section

--- src/App.js
+++ src/App.js
@@ -1,36 +1,33 @@
-import React, { useState } from 'react';
+import React, {Component} from 'react';
 
-import Jumbotron from "react-bootstrap/Jumbotron";
-import Toast from "react-bootstrap/Toast";
-import Container from 'react-bootstrap/Container';
+import Intro from './Intro.js'
+// import Jumbotron from "react-bootstrap/Jumbotron";
+// import Toast from "react-bootstrap/Toast";
+// import Container from 'react-bootstrap/Container';
 
 import './App.css';
 
-const ExampleToast = ({ children }) => {
-  const [show, toggleShow] = useState(true);
+// const ExampleToast = ({ children }) => {
+//   const [show, toggleShow] = useState(true);
 
-  return (
-    <Toast show={show} onClose={() => toggleShow(!show)}>
-      <Toast.Header>
-        <strong className="mr-auto">React-Bootstrap</strong>
-      </Toast.Header>
-      <Toast.Body>{children}</Toast.Body>
-    </Toast>
-  );
+//   return (
+//     <Toast show={show} onClose={() => toggleShow(!show)}>
+//       <Toast.Header>
+//         <strong className="mr-auto">React-Bootstrap</strong>
+//       </Toast.Header>
+//       <Toast.Body>{children}</Toast.Body>
+//     </Toast>
+//   );
+// };
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Intro />
+      </div>
+    )
+  };
 };
-
-const App = () => (
-  <Container className="p-3">
-    <Jumbotron>
-      <h1 className="header">Welcome To React-Bootstrap</h1>
-      <ExampleToast className="toast">
-        We now have Toasts
-        <span role="img" aria-label="tada">
-          🎉
-        </span>
-      </ExampleToast>
-    </Jumbotron>
-  </Container>
-);
 
 export default App;
---
+++ src/Intro.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Intro.css";
+
+const Intro = () => {
+  return (
+    <div>
+      <div className="introduction section">
+        <h4 className="serif">Hey! My name is</h4>
+        <h1 className="bold">Adam Ali</h1>
+        <h3 className="important serif subtitle">I'm a developer.</h3>
+      </div>
+    </div>
+  );
+};
+
+export default Intro;
