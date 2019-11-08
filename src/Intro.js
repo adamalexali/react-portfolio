73650012701c4e52623121f751b2bd5b45479ea6
Adam Ali <adamalexali@Adams-MacBook-Pro.local>
2019-11-07 23:31:58 -0500

    added animations to intro

--- src/Intro.js
+++ src/Intro.js
@@ -1,19 +1,18 @@
 import React from "react";
-import ScrollAnimation from "react-animate-on-scroll";
 
 import "./Intro.css";
 
 const Intro = () => {
   return (
-    <ScrollAnimation animateIn='fadeIn'>
     <div>
       <div className="introduction section">
-        <h4 className="serif">Hey! My name is</h4>
-        <h1 className="bold">Adam Ali</h1>
-        <h3 className="important serif subtitle">I'm a developer.</h3>
+        <h4 className="serif animated fadeIn">Hey! My name is</h4>
+        <h1 className="bold animated fadeInRight delay-1s">Adam Ali</h1>
+        <h3 className="important serif subtitle animated fadeIn delay-2s">
+          I'm a developer.
+        </h3>
       </div>
     </div>
-    </ScrollAnimation>
   );
 };
 
