66e54bcf0e6d391d149bfd8bfb13e79e2d91db70
Adam Ali <adamalexali@Adams-MacBook-Pro.local>
2020-07-25 11:47:43 -0400

    cleaning up before rework.

--- src/ProjectCard.js
+++ src/ProjectCard.js
@@ -1,10 +1,12 @@
-import React from "react";
-import "./ProjectCard.css";
+import React from 'react';
+import './ProjectCard.css';
 
 const ProjectCard = ({ title, img }) => {
   return (
-    <a target="_top" href={`/#/${title}`}>
-      <div className="cardContainer">
+    // if using HashRouter instead of BrowserRouter, change this line:
+    // href={`/#/${title}`}
+    <a target='_top' href={`/${title}`}>
+      <div className='cardContainer'>
         <div>
           <img src={img} alt={title}></img>
         </div>
