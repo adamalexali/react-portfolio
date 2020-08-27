b1d51706c58d3530c5731025579d0f3c79f55b68
Adam <adamalexali@gmail.com>
2020-08-27 13:57:09 -0400

    tweaking deployment

--- src/Home.js
+++ src/Home.js
@@ -1,22 +1,22 @@
-import React from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "animate.css/animate.min.css";
-import "./Home.css";
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'animate.css/animate.min.css';
+import './Home.css';
 
 // import Navbar from "react-bootstrap/Navbar";
 // import Nav from "react-bootstrap/Nav";
-import ProjectCard from "./ProjectCard";
-import ScrollAnimation from "react-animate-on-scroll";
+import ProjectCard from './ProjectCard';
+import ScrollAnimation from 'react-animate-on-scroll';
 // import Bounce from "./StyledAnimations";
 
-import modpodImg from "./images/projectCards/modpodImg.png";
-import bladeImg from "./images/projectCards/bladeImg.png";
-import brandingImg from "./images/projectCards/brandingImg.png";
-import portfolioImg from "./images/projectCards/portfolioImg.png";
+import modpodImg from './images/projectCards/modpodImg.png';
+import bladeImg from './images/projectCards/bladeImg.png';
+import brandingImg from './images/projectCards/brandingImg.png';
+import portfolioImg from './images/projectCards/portfolioImg.png';
 
 const Home = () => {
   return (
-    <div className="appContainer">
+    <div className='appContainer'>
       {/* <div>
         <Navbar
           sticky="top"
@@ -51,22 +51,22 @@ const Home = () => {
           </Navbar.Collapse>
         </Navbar>
       </div> */}
-      <div className="introduction section">
-        <h4 className="serif animated fadeIn">Hey! My name is</h4>
-        <h1 className="bold animated fadeInRight">Adam Ali</h1>
-        <h3 className="important serif subtitle animated fadeIn">
+      <div className='introduction section'>
+        <h4 className='serif animated fadeIn'>Hello, my name is</h4>
+        <h1 className='bold animated fadeInRight'>Adam Ali</h1>
+        <h3 className='important serif subtitle animated fadeIn'>
           Designer & developer
         </h3>
       </div>
-      <div className="project section">
+      <div className='project section'>
         <div>
-          <ScrollAnimation animateIn="fadeIn" animateOnce="true">
-            <ProjectCard title="ModPod" img={modpodImg} alt="ModPod" />
+          <ScrollAnimation animateIn='fadeIn' animateOnce='true'>
+            <ProjectCard title='ModPod' img={modpodImg} alt='ModPod' />
           </ScrollAnimation>
         </div>
         <div>
-          <ScrollAnimation animateIn="fadeIn" animateOnce="true">
-            <ProjectCard title="Blade" img={bladeImg} alt="Blade" />
+          <ScrollAnimation animateIn='fadeIn' animateOnce='true'>
+            <ProjectCard title='Blade' img={bladeImg} alt='Blade' />
           </ScrollAnimation>
         </div>
         {/* <div>
@@ -81,54 +81,54 @@ const Home = () => {
         </div> */}
       </div>
       <footer>
-        <div class="section contact">
-          <ScrollAnimation delay="500" animateIn="fadeInUp" animateOnce="true">
+        <div class='section contact'>
+          <ScrollAnimation delay='500' animateIn='fadeInUp' animateOnce='true'>
             <h4>@adamalexali</h4>
             <div>
               <ul>
                 <li>
                   <a
-                    className="sm-link"
-                    target="_blank"
-                    href="https://www.linkedin.com/in/adamalexali/"
+                    className='sm-link'
+                    target='_blank'
+                    href='https://www.linkedin.com/in/adamalexali/'
                   >
-                    <i class="fab fa-linkedin"></i>
+                    <i class='fab fa-linkedin'></i>
                   </a>
                 </li>
                 <li>
                   <a
-                    className="sm-link"
-                    target="_blank"
-                    href="https://github.com/adamalexali"
+                    className='sm-link'
+                    target='_blank'
+                    href='https://github.com/adamalexali'
                   >
-                    <i class="fab fa-github"></i>
+                    <i class='fab fa-github'></i>
                   </a>
                 </li>
                 <li>
                   <a
-                    className="sm-link"
-                    target="_blank"
-                    href="mailto:adamalexali@gmail.com"
+                    className='sm-link'
+                    target='_blank'
+                    href='mailto:adamalexali@gmail.com'
                   >
-                    <i className="fab fa-google"></i>
+                    <i className='fab fa-google'></i>
                   </a>
                 </li>
                 <li>
                   <a
-                    className="sm-link"
-                    target="_blank"
-                    href="https://www.behance.net/adamalexali"
+                    className='sm-link'
+                    target='_blank'
+                    href='https://www.behance.net/adamalexali'
                   >
-                    <i className="fab fa-behance"></i>
+                    <i className='fab fa-behance'></i>
                   </a>
                 </li>
                 <li>
                   <a
-                    className="sm-link"
-                    target="_blank"
-                    href="https://www.instagram.com/adamalexali/"
+                    className='sm-link'
+                    target='_blank'
+                    href='https://www.instagram.com/adamalexali/'
                   >
-                    <i className="fab fa-instagram"></i>
+                    <i className='fab fa-instagram'></i>
                   </a>
                 </li>
               </ul>
