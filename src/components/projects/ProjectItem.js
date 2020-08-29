c55202366802ae6ef1a7dc6af51a95d2df480f33
Adam <adamalexali@gmail.com>
2020-08-29 19:51:10 -0400

    working on modpod redesign

--- src/components/projects/ProjectItem.js
+++ src/components/projects/ProjectItem.js
@@ -6,7 +6,7 @@ export default function ProjectItem() {
   // So Rellax works... but I'm not sure why. I found this solution on stack overflow, and the poster said it is achieved using Hooks, which I don't know much about
   // looks like useRef isn't used at all, I'll try removing it and seeing if theres much effect.
   // Also just not really sure what refs are. useEffect() seems to be how it works.
-  const rellaxRef = useRef();
+  // const rellaxRef = useRef();
 
   // so I guess, creating a new Rellax animation, set a class name and parameters. Speed is the one probably most used. Z-index I guess set in css file.
   // So what I can do is set ~3 Rellax animations, maybe for foreground, midground, and background. Set the speeds and z-index. Shouldn't be too crazy.
--- src/components/projects/portfolioPieces/Modpod.js
+++ src/components/projects/portfolioPieces/Modpod.js
@@ -1,14 +1,52 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
+import Rellax from 'rellax';
+import modpodBanner from '../../../../src/images/modpod/modpodBanner.png';
 
-class Modpod extends Component {
-  render() {
-    return (
+export default function Modpod() {
+  useEffect(() => {
+    new Rellax('.animate', {
+      speed: -2,
+      center: false,
+      wrapper: null,
+      round: true,
+      vertical: true,
+      horizontal: false,
+    });
+
+    new Rellax('.animate2', {
+      speed: 2,
+      center: false,
+      wrapper: null,
+      round: true,
+      vertical: true,
+      horizontal: false,
+    });
+
+    new Rellax('.animate3', {
+      speed: 20,
+      center: false,
+      wrapper: null,
+      round: true,
+      vertical: true,
+      horizontal: false,
+    });
+
+    // new Rellax(rellaxRef.current, {
+    //   speed: -10,
+    //   center: false,
+    //   wrapper: null,
+    //   round: true,
+    //   vertical: true,
+    //   horizontal: false,
+    // });
+  }, []);
+  return (
+    <div className='container'>
       <div>
-        <h1>Modpod Immersive Environment</h1>
-        <p>The incredible VR experience!</p>
+        <h1>Modpod</h1>
+        <h2>Immersive environment</h2>
+        <img src='' alt='' />
       </div>
-    );
-  }
+    </div>
+  );
 }
-
-export default Modpod;
